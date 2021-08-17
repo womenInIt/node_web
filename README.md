42e58607e6cbbdfa6ad7d059e24224a7990b055d
fialo4ka <tona16@inbox.lv>
2021-08-17 20:53:53 +0200

    begin

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-# node_web
+# How to run
 
+`npm install`
+`npm run dev`
 
-npm install
-
-
-npm install express-oas-generator
-
-
-const expressOasGenerator = require('express-oas-generator');
-expressOasGenerator.init(app, {}); 
-
-
+To view swagger page:
 http://localhost:4520/api-docs/
-
-
-
-http://localhost:4520/?a=3&b=5
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require('express');
+const expressOasGenerator = require('express-oas-generator');
 const app = express()
 const port = 4520; 
+
+expressOasGenerator.init(app, {}); 
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
   });
 
 app.get('/', (request, response) => {
-
-
-
+  response.send("Hello from server");
 })
